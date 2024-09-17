d6a530fe8c8a8b1e04e0b5144bf7ea3049b35afb
adindawrdhnk <adwrdhnk@gmail.com>
2024-09-17 23:01:14 +0700

    push ke ows otomatis

--- local_finds/settings.py
+++ local_finds/settings.py
@@ -125,3 +125,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://adinda-maharani31-localfinds.pbp.cs.ui.ac.id/", "https://adinda-maharani31-localfinds.pbp.cs.ui.ac.id/"]
+
+
